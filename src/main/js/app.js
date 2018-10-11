eef7f8f821430ab9cc5567cf42dfd16226e9d757
Frauke Trautmann <frauke.trautmann@dreamit.de>
2018-10-11 16:25:38 +0200

    setup frontend

---
+++ src/main/js/app.js
@@ -0,0 +1,58 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const client = require('./client');
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {listEntry: []};
+    }
+
+    componentDidMount() {
+        client({method: 'GET', path: '/api/listEntry'}).done(response => {
+            this.setState({listEntry: response.entity._embedded.listEntry});
+    });
+    }
+
+    render() {
+        return (
+            <ListEntries listEntry={this.state.listEntry}/>
+    )
+    }
+}
+
+class ListEntries extends React.Component{
+    render() {
+        const listEntry = this.props.listEntry.map(listEntry =>
+            <listEntry key={listEntry._links.self.href} employee={listEntry}/>
+        );
+        return (
+            <table>
+                <tbody>
+                <tr>
+                    <th>What to do</th>
+                    <th>is fulfilled</th>
+                </tr>
+                {listEntry}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+class ListEntry extends React.Component{
+    render() {
+        return (
+            <tr>
+                <td>{this.props.listEntry.listItem}</td>
+                <td>{this.props.listEntry.isFulfilled}</td>
+            </tr>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('react')
+);
